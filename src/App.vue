ee9a5866d28d805dc5d2faefb98a807058bebca6
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2025-01-09 20:29:03 -0500

    not sure how I feel about the design
    the background photo is part of the problem

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="navigation">
+    <img alt="Vue logo" class="logo" src="/ProfilePic.jpg" width="250" height="250" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Welcome to my website!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/portfolio">Portfolio</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView class="content" />
+</template>
+
+<style scoped>
+
+.content {
+  margin-left: 10%;
+}
+header {
+  line-height: .5;
+  max-height: 100vh;
+}
+
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,25 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+  <h2 class="introduction">
+    Hi, I’m Daniel Christian Diaz—a passionate web developer with 4 years of experience building engaging and user-friendly digital experiences. My work combines creativity, problem-solving, and technical expertise to bring ideas to life, whether it’s crafting dynamic web applications or solving complex challenges with clean, efficient code.
+    Take a look at my portfolio to explore the projects I’ve worked on, from responsive web designs to interactive applications. If you’re looking for a developer who’s detail-oriented, driven, and ready to take on new challenges, let’s connect!
+  </h2>
+</template>
+
+<style scoped>
+  .introduction {
+    color: #FFD700;
+    background-color: grey;
+    padding: 1rem;
+  }
+</style>
