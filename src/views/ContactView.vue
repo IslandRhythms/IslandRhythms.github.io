685ef9291f8af2580eb71933e73f97afd8b0bea2
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2025-01-12 20:27:47 -0500

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label">Email address</label>
-    <input
-      type="email"
-      class="form-control"
-      id="exampleFormControlInput1"
-      placeholder="name@example.com"
-    />
+  <div class="d-flex justify-content-center">
+    Have questions, ideas, or just want to say hi? Fill out the form below, and I’ll get back to you
+    as soon as possible. I’m looking forward to hearing from you!
   </div>
-  <div class="mb-3">
-    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
+  <form action="https://formspree.io/f/xannvqvp" method="POST" class="container mt-4">
+    <div class="mb-3">
+      <label for="email" class="form-label">Your email:</label>
+      <input type="email" name="email" id="email" class="form-control" required />
+    </div>
+    <div class="mb-3">
+      <label for="message" class="form-label">Your message:</label>
+      <textarea name="message" id="message" class="form-control" rows="4" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Send</button>
+  </form>
 </template>
